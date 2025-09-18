c96be623889b99e54f2586689e5e386fb1d31dab
TheKingOfTheHats <Danielm8212@gmail.com>
2025-09-18 22:30:52 +0300

    addad DischargeSubsystem and started working on DischargeCommands

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/decodeCommands/DischargeCommands.java
@@ -0,0 +1,14 @@
+package org.firstinspires.ftc.teamcode.decodeCommands;
+
+import com.qualcomm.robotcore.robocol.Command;
+import com.seattlesolvers.solverslib.command.CommandBase;
+
+import org.firstinspires.ftc.teamcode.decodeSubsystems.DischargeSubsystem;
+
+public class DischargeCommands {
+    public static class SetFlyWheelMotor extends CommandBase {
+        DischargeSubsystem DischargeSubsystem;
+
+
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/decodeSubsystems/DischargeSubsystem.java
@@ -0,0 +1,35 @@
+package org.firstinspires.ftc.teamcode.decodeSubsystems;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.HardwareDevice;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.seattlesolvers.solverslib.command.SubsystemBase;
+
+public class DischargeSubsystem extends SubsystemBase {
+
+    private final DcMotorEx flyWheelMotor, turretMotor;
+    private final Servo rampServo;
+
+
+    public DischargeSubsystem(HardwareMap hm) {
+        flyWheelMotor = hm.get(DcMotorEx.class, "flyWheelMotor");
+        turretMotor = hm.get(DcMotorEx.class, "turretMotor");
+        turretMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        turretMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rampServo = hm.get(Servo.class, "rampServo");
+    }
+
+    public void setFlyWheelPower(double flyWheelPower) {
+        flyWheelMotor.setPower(flyWheelPower);
+    }
+
+    public void setTurretPower(double turretPower) {
+        turretMotor.setPower(turretPower);
+    }
+    public void setRampPos(double rampPos) {
+        rampServo.setPosition(rampPos);
+    }
+
+}

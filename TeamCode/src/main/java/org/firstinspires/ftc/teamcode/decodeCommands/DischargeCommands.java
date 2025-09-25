c898406c60f596b5d665754cbd13307544669bbf
TheKingOfTheHats <Danielm8212@gmail.com>
2025-09-25 20:00:02 +0300

    added GoTo command to all Discharge

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/decodeCommands/DischargeCommands.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/decodeCommands/DischargeCommands.java
@@ -1,11 +1,12 @@
 package org.firstinspires.ftc.teamcode.decodeCommands;
 
+import com.acmerobotics.dashboard.config.Config;
 import com.seattlesolvers.solverslib.command.CommandBase;
 
 import org.firstinspires.ftc.teamcode.decodeSubsystems.DischargeSubsystem;
 
 import java.util.function.Supplier;
-
+@Config
 public class DischargeCommands {
     public static class DischargeManual extends CommandBase {
         DischargeSubsystem dischargeSubsystem;
@@ -25,9 +26,31 @@ public class DischargeCommands {
             dischargeSubsystem.setFlyWheelPower(flyWheelPower.get());
             dischargeSubsystem.setTurretPower(turretPower.get());
         }
+    }
+    public static class GoTo extends CommandBase {
+        DischargeSubsystem dischargeSubsystem;
+        double turretPower;
+        double flyWheelPower;
+        double rampDegree;
+        double wantedPos;
+        static final double kp = 0.01;
+
+        public GoTo(DischargeSubsystem dischargeSubsystem,  double flyWheelPower, double turretPos, double rampDegree){
+            this.dischargeSubsystem = dischargeSubsystem;
+            this.flyWheelPower = flyWheelPower;
+            this.rampDegree = rampDegree;
+            wantedPos = turretPos;
+            addRequirements(dischargeSubsystem);
+        }
+
+        @Override
+        public void execute() {
+            double power = (wantedPos - dischargeSubsystem.getTurretPosition()) * kp;
+            dischargeSubsystem.setTurretPower(power);
+            dischargeSubsystem.setFlyWheelPower(flyWheelPower);
+            dischargeSubsystem.setRampDegree(rampDegree);
 
 
-
-
+        }
     }
 }

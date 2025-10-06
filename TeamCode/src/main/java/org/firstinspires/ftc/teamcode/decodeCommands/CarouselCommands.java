c431608dba5955c9539d314f5cb253f8c2452021
Ori Supino <ori@supino.org>
2025-10-06 20:27:25 +0300

    added sorting by motif

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/decodeCommands/CarouselCommands.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/decodeCommands/CarouselCommands.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode.decodeCommands;
 
 import com.seattlesolvers.solverslib.command.CommandBase;
 import org.firstinspires.ftc.teamcode.decodeSubsystems.CarouselSubsystem;
+import org.firstinspires.ftc.teamcode.decodeSubsystems.Motif;
 
 import java.util.function.Supplier;
 
@@ -59,7 +60,6 @@ public class CarouselCommands {
 
     public static class ThirdOfSpin extends CommandBase {
         private final CarouselSubsystem carouselSubsystem;
-        double power;
         int currentPos;
         int tagetPos;
         double kp = 0.1;
@@ -94,8 +94,6 @@ public class CarouselCommands {
 
     public static class GreenBallToBackSlot extends CommandBase {
         private final CarouselSubsystem carouselSubsystem;
-        private final double kp = 0.1;
-        private final int tolerance = 50;
         private int targetPos;
 
         public GreenBallToBackSlot(CarouselSubsystem carouselSubsystem) {
@@ -115,6 +113,7 @@ public class CarouselCommands {
         public void execute() {
             int currentPos = (int) carouselSubsystem.getPosition();
             double error = targetPos - currentPos;
+            double kp = 0.1;
             double power = kp * error;
 
             carouselSubsystem.setSpinPower(power);
@@ -123,12 +122,62 @@ public class CarouselCommands {
         @Override
         public boolean isFinished() {
             int currentPos = (int) carouselSubsystem.getPosition();
+            int tolerance = 50;
             return Math.abs(currentPos - targetPos) < tolerance;
         }
 
         @Override
         public void end(boolean interrupted) {
             carouselSubsystem.setSpinPower(0);
+        }
+    }
+    public static class SortByMotif extends CommandBase {
+        CarouselSubsystem carouselSubsystem;
+        private final Motif motif;
+        int steps;
+        int targetPos;
+        double kp = 0.1;
+        boolean completion = false;
+
+        public SortByMotif (Motif motif, int steps, boolean completion) {
+            this.motif = motif;
+            this.steps = steps;
+            this.completion = completion;
+        }
+
+        @Override
+        public void initialize() {
+            // sets step count for initial movement
+            // positive is clockwise and the other way around
+            switch (motif) {
+                case GPP: steps = 1; break;
+                case PGP: steps = 0; break;
+                case PPG: steps = -1; break;
+            }
+        }
+
+        // helper func:
+        public void moveByThirds (int steps) {
+            int currentPos = (int) carouselSubsystem.getPosition();
+            while (Math.abs(targetPos-currentPos) < 50) {
+                targetPos = (int) (carouselSubsystem.getPosition() + steps * carouselSubsystem.spinConversion);
+                currentPos = (int) carouselSubsystem.getPosition();
+                double error = targetPos - currentPos;
+                double power = kp * error;
+
+                carouselSubsystem.setSpinPower(power);
+            }
+        }
+        @Override
+        public void execute() {
+            moveByThirds(steps);
+            moveByThirds(3);
+            completion = true; // completes after the 2 loops
+        }
+
+        @Override
+        public boolean isFinished() {
+            return completion;
         }
     }
 }
